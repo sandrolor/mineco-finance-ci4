71c2b7d81a147f26aaac1986040d30c92d8f33a9
Sandro <sandrolor@gmail.com>
2024-12-01 15:56:37 -0300

    Rotas para Contas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,3 +20,12 @@ $routes->group('grupocontas', ['filter' => 'auth'], function ($routes) {
     $routes->post('update/(:num)', 'GrupoContas::update/$1');
     $routes->get('delete/(:num)', 'GrupoContas::delete/$1');
 });
+
+$routes->group('contas', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'Contas::index');              // Listagem de contas
+    $routes->get('create', 'Contas::create');       // Formulário de criação
+    $routes->post('store', 'Contas::store');        // Salvar nova conta
+    $routes->get('edit/(:num)', 'Contas::edit/$1'); // Formulário de edição
+    $routes->post('update/(:num)', 'Contas::update/$1'); // Atualizar conta
+    $routes->get('delete/(:num)', 'Contas::delete/$1');  // Excluir conta
+});
