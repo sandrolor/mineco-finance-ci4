606e3af44fa562e2f54ef3d049560a8c7bffa864
Sandro <sandrolor@gmail.com>
2024-12-01 15:40:43 -0300

    Rotas para Grupo Conta

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,12 @@ $routes->post('auth/login', 'Auth::login');
 $routes->get('auth/logout', 'Auth::logout');
 
 $routes->get('dashboard', 'Dashboard::index', ['filter' => 'auth']);
+
+$routes->group('grupocontas', ['filter' => 'auth'], function ($routes) {
+    $routes->get('/', 'GrupoContas::index');
+    $routes->get('create', 'GrupoContas::create');
+    $routes->post('store', 'GrupoContas::store');
+    $routes->get('edit/(:num)', 'GrupoContas::edit/$1');
+    $routes->post('update/(:num)', 'GrupoContas::update/$1');
+    $routes->get('delete/(:num)', 'GrupoContas::delete/$1');
+});
