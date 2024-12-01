207c226f74995659f2a4dfc23bc9017dd801fd37
Sandro <sandrolor@gmail.com>
2024-12-01 15:24:27 -0300

    Configure as rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Auth::login');
+$routes->get('login', 'Auth::login');
+$routes->post('auth/login', 'Auth::login');
+$routes->get('auth/logout', 'Auth::logout');
+
+$routes->get('dashboard', 'Dashboard::index', ['filter' => 'auth']);
