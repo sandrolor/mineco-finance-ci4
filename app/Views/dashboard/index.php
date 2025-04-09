3a05e82c680f8f027ad28f188106b2bb4ba0a693
Sandro <sandrolor@gmail.com>
2025-04-09 20:13:30 -0300

    alt dashboard

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,9 +1,9 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('content') ?>
 <div class="container my-5">
-    <div class="p-5 bg-light rounded">
-        <h2 class="text-center">Bem-vindo, <?= session('username') ?>!</h2>
-        <h3 class="text-center">Sistema de Controle Financeiro.</h3>
+    <div class="p-5 bg-light rounded text-center">
+        <h2>Bem-vindo, <?= session('username') ?>!</h2>
+        <h3>Sistema de Controle Financeiro.</h3>
     </div>
 </div>
 <?= $this->endSection() ?>
