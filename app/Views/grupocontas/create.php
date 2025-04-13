cf6bb95d33943e3add0ae2dc386bdc412cada3bd
Sandro <sandrolor@gmail.com>
2025-04-13 11:14:34 -0300

    flash

--- app/Views/grupocontas/create.php
+++ app/Views/grupocontas/create.php
@@ -3,6 +3,13 @@
 
 <div class="container mt-5">
     <h2>Criar Grupo de Contas</h2>
+    <?php if (session()->getFlashdata('errors')): ?>
+        <div class="alert alert-danger">
+            <?php foreach (session()->getFlashdata('errors') as $error): ?>
+                <?= $error ?><br>
+            <?php endforeach ?>
+        </div>
+    <?php endif; ?>
     <form method="post" action="<?= site_url('grupocontas/store') ?>">
         <?= csrf_field() ?>
         <div class="mb-3">
